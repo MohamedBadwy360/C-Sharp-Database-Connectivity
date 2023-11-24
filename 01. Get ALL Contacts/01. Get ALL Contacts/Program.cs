b8a42f50dddf03dd98867cdcc27c6e233da3ad23
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-24 18:28:22 +0200

    Get All Contacts

---
+++ 01. Get ALL Contacts/01. Get ALL Contacts/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _01.Get_ALL_Contacts
+{
+    internal class Program
+    {
+        static string connectionString = "Server = .; Database = ContactsDB; User Id = sa; Password = sa123456";
+
+        static void PrintDatabase()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "select * from Contacts";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {              
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int contactID = (int)reader["ContactID"];
+                    string firstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    string email = (string)reader["Email"];
+                    string phone = (string)reader["Phone"];
+                    string address = (string)reader["Address"];
+                    int countryID = (int)reader["CountryID"];
+
+                    Console.WriteLine($"ContactID : {contactID}");
+                    Console.WriteLine($"FirstName : {firstName}");
+                    Console.WriteLine($"LastName : {lastName}");
+                    Console.WriteLine($"Email : {email}");
+                    Console.WriteLine($"Phone : {phone}");
+                    Console.WriteLine($"Address : {address}");
+                    Console.WriteLine($"CountryID : {countryID}");
+                    Console.WriteLine();
+                }
+
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error. {ex.Message}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintDatabase();
+        }
+    }
+}
