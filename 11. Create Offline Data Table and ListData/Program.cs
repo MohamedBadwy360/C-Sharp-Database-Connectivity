ef838042b2c628ee433fe3e8996332f15b25940c
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-26 01:11:02 +0200

    DataTable Example 1 (Create Offline Data Table and ListData)

---
+++ 11. Create Offline Data Table and ListData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace _11.Create_Offline_Data_Table_and_ListData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable EmployeesDataTable = new DataTable();
+
+            // Add Columns
+            EmployeesDataTable.Columns.Add("ID", typeof(int));
+            EmployeesDataTable.Columns.Add("Name", typeof(string));
+            EmployeesDataTable.Columns.Add("Country", typeof(string));
+            EmployeesDataTable.Columns.Add("Salary", typeof(double));
+            EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+
+            // Add Values
+            EmployeesDataTable.Rows.Add(1, "Mohamed Badwy", "Egypt", 10000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(2, "Elsaid Badwy", "Egypt", 20000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(3, "Asmaa Badwy", "Egypt", 30000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(4, "Mahasen Badwy", "Egypt", 40000, DateTime.Now);
+
+            foreach(DataRow row in EmployeesDataTable.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}\t Name: {row["Name"]}\t Country: {row["Country"]}\t Salary: {row["Salary"]}\t Date: {row["Date"]}");
+            }
+        }
+    }
+}
