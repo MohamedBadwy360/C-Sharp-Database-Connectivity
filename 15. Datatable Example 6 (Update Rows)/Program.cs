e6a0ef42300a6e1c0a653334a6ca132810847d73
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-26 14:22:42 +0200

    Update Rows

---
+++ 15. Datatable Example 6 (Update Rows)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+
+
+namespace _15.Datatable_Example_6__Update_Rows_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable EmployeesDataTable = new DataTable();
+
+            // Add Columns
+            EmployeesDataTable.Columns.Add("ID", typeof(int));
+            EmployeesDataTable.Columns.Add("Name", typeof(string));
+            EmployeesDataTable.Columns.Add("Country", typeof(string));
+            EmployeesDataTable.Columns.Add("Salary", typeof(double));
+            EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+
+            // Add Values
+            EmployeesDataTable.Rows.Add(1, "Mohamed Badwy", "Egypt", 10000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(2, "Elsaid Badwy", "Egypt", 20000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(3, "Asmaa Badwy", "Egypt", 30000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(4, "Mahasen Badwy", "Egypt", 40000, DateTime.Now);
+
+            foreach (DataRow row in EmployeesDataTable.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}\t Name: {row["Name"]}\t Country: {row["Country"]}\t Salary: {row["Salary"]}\t Date: {row["Date"]}");
+            }
+
+            Console.WriteLine("\n\nAfter Updating\n");
+
+            // Updating rows
+            DataRow[] rowsResult = EmployeesDataTable.Select("ID = 4");
+
+            foreach (DataRow row in rowsResult)
+            {
+                row["Name"] = "Hanem Shawky";
+                row["Salary"] = 30000;
+            }
+
+            foreach (DataRow row in EmployeesDataTable.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}\t Name: {row["Name"]}\t Country: {row["Country"]}\t Salary: {row["Salary"]}\t Date: {row["Date"]}");
+            }
+
+            // ============================================
+
+            // Clear DataTable
+
+            //EmployeesDataTable.Clear();
+            //foreach (DataRow row in EmployeesDataTable.Rows)
+            //{
+            //    Console.WriteLine($"ID: {row["ID"]}\t Name: {row["Name"]}\t Country: {row["Country"]}\t Salary: {row["Salary"]}\t Date: {row["Date"]}");
+            //}
+        }
+    }
+}
