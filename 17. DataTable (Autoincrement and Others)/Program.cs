aa3c9bfc23d34409298f262fc64297b980616423
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-26 14:45:04 +0200

    Auto Increment

--- 17. DataTable (Autoincrement and Others)/Program.cs
+++ 17. DataTable (Autoincrement and Others)/Program.cs
@@ -11,17 +11,82 @@ namespace _17.DataTable__Autoincrement_and_Others_
             DataTable EmployeesDataTable = new DataTable();
 
             // Add Columns
-            EmployeesDataTable.Columns.Add("ID", typeof(int));
-            EmployeesDataTable.Columns.Add("Name", typeof(string));
-            EmployeesDataTable.Columns.Add("Country", typeof(string));
-            EmployeesDataTable.Columns.Add("Salary", typeof(double));
-            EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+            //EmployeesDataTable.Columns.Add("ID", typeof(int));
+            //EmployeesDataTable.Columns.Add("Name", typeof(string));
+            //EmployeesDataTable.Columns.Add("Country", typeof(string));
+            //EmployeesDataTable.Columns.Add("Salary", typeof(double));
+            //EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+
+            DataColumn dtColumn = new DataColumn();
+
+            dtColumn.DataType = typeof(int);
+            dtColumn.ColumnName = "ID";
+            dtColumn.AutoIncrement = true;
+            dtColumn.AutoIncrementSeed = 1;
+            dtColumn.AutoIncrementStep = 1;
+
+            dtColumn.Caption = "Employee ID";
+            dtColumn.ReadOnly = true;
+            dtColumn.Unique = true;
+            EmployeesDataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn()
+            {
+                DataType = typeof(string),
+                ColumnName = "Name",
+                AutoIncrement = false,
+                Caption = "Name",
+                ReadOnly = false,
+                Unique = false
+            };
+
+            EmployeesDataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn()
+            {
+                DataType = typeof(string),
+                ColumnName = "Country",
+                AutoIncrement = false,
+                Caption = "Country",
+                ReadOnly = false,
+                Unique = false
+            };
+
+            EmployeesDataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn()
+            {
+                DataType = typeof(double),
+                ColumnName = "Salary",
+                AutoIncrement = false,
+                Caption = "Salary",
+                ReadOnly = false,
+                Unique = false
+            };
+
+            EmployeesDataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn()
+            {
+                DataType = typeof(DateTime),
+                ColumnName = "Date",
+                AutoIncrement = false,
+                Caption = "Date",
+                ReadOnly = false,
+                Unique = false
+            };
+
+            EmployeesDataTable.Columns.Add(dtColumn);
+
+            DataColumn[] primaryKey = new DataColumn[1];
+            primaryKey[0] = EmployeesDataTable.Columns["ID"];
+            EmployeesDataTable.PrimaryKey = primaryKey;
 
             // Add Values
-            EmployeesDataTable.Rows.Add(1, "Mohamed Badwy", "Egypt", 10000, DateTime.Now);
-            EmployeesDataTable.Rows.Add(2, "Elsaid Badwy", "Egypt", 20000, DateTime.Now);
-            EmployeesDataTable.Rows.Add(3, "Asmaa Badwy", "Egypt", 30000, DateTime.Now);
-            EmployeesDataTable.Rows.Add(4, "Mahasen Badwy", "Egypt", 40000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(null, "Mohamed Badwy", "Egypt", 10000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(null, "Elsaid Badwy", "Egypt", 20000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(null, "Asmaa Badwy", "Egypt", 30000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(null, "Mahasen Badwy", "Egypt", 40000, DateTime.Now);
 
             Console.WriteLine("Employees List\n");
             foreach (DataRow row in EmployeesDataTable.Rows)
